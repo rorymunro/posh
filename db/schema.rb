95c5ac53a547b306a3f8f61ddc5c7aadf546c9e7
rory <rorymunro@hotmail.com>
2014-06-04 16:43:04 +0100

    fix database

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140604134224) do
     t.string   "remember_token"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
