bcfcf4f9f1f21ddd14bda886569fcbbe3113b490
rory <rorymunro@hotmail.com>
2014-06-05 18:36:36 +0100

    add user destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index]
+  
+  
+  def index
+    @users = User.all
+  end
   
   def show
   	@user = User.find(params[:id])
@@ -6,6 +12,12 @@ class UsersController < ApplicationController
 
   def new
   	@user = User.new
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "They are a gonna"
+    redirect_to users_url
   end
 
   def create
@@ -25,4 +37,16 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+  end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
 end
